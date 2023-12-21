a448a96aa8b61261b106bfe3a2b1fffbe5a91d45
kerolosragaie <kerolosragaie@gmail.com>
2023-12-21 17:11:54 +0200

    fixing CustomTextField

--- app/src/main/java/com/kerollosragaie/appvalidation/core/components/CustomTextField.kt
+++ app/src/main/java/com/kerollosragaie/appvalidation/core/components/CustomTextField.kt
@@ -4,9 +4,11 @@ import androidx.annotation.StringRes
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -27,6 +29,7 @@ import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
@@ -116,8 +119,9 @@ fun CustomTextField(
             Text(
                 modifier = Modifier
                     .fillMaxWidth(0.8f)
-                    .align(Alignment.End)
                     .padding(top = 10.dp),
+                maxLines = 2,
+                overflow = TextOverflow.Ellipsis,
                 text = stringResource(id = state.errorMessageId),
                 color = MaterialTheme.colorScheme.error,
                 style = MaterialTheme.typography.titleSmall,
