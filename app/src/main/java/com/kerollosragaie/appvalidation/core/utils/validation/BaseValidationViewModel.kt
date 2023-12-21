006c49f0697ec6c72c57e3abd1830bd44d1ca08a
kerolosragaie <kerolosragaie@gmail.com>
2023-12-22 00:34:07 +0200

    cleaning code

--- app/src/main/java/com/kerollosragaie/appvalidation/core/utils/validation/BaseValidationViewModel.kt
+++ app/src/main/java/com/kerollosragaie/appvalidation/core/utils/validation/BaseValidationViewModel.kt
@@ -13,7 +13,6 @@ import com.kerollosragaie.appvalidation.core.utils.validation.usecase.ValidatePa
 import com.kerollosragaie.appvalidation.core.utils.validation.usecase.ValidateText
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
--- app/src/main/java/com/kerollosragaie/appvalidation/features/auth/presentation/signin/SignInScreen.kt
+++ app/src/main/java/com/kerollosragaie/appvalidation/features/auth/presentation/signin/SignInScreen.kt
@@ -16,7 +16,6 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -26,7 +25,6 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.kerollosragaie.appvalidation.R
 import com.kerollosragaie.appvalidation.core.components.CustomButton
 import com.kerollosragaie.appvalidation.core.theme.AppValidationTheme
@@ -34,9 +32,6 @@ import com.kerollosragaie.appvalidation.core.components.CustomTextField
 import com.kerollosragaie.appvalidation.core.components.TextFieldType
 import com.kerollosragaie.appvalidation.core.utils.validation.event.ValidationEvent
 import com.kerollosragaie.appvalidation.core.utils.validation.event.ValidationResultEvent
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.flow.take
 
 @Composable
 fun SignInScreen(
